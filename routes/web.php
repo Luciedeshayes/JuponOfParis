4cf17730c1751d453c6065a0e54bb4891cbe7928
Luciedeshayes <49715241+Luciedeshayes@users.noreply.github.com>
2021-11-30 20:15:46 +0100

    feature/first-commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+if( env( 'SHOP_MULTILOCALE' ) )
+{
+    $locale = ['prefix' => '{locale}', 'where' => ['locale' => '[a-zA-Z]{2}(\_[a-zA-Z]{2})?']];
+
+    Route::get('/', function () {
+        return redirect(app()->getLocale());
+    });
+}
+
+
+Route::group($locale ?? [], function() {
+
+    // only if SHOP_MULTILOCALE isn't enabled due to restrictions in Laravel
+    Auth::routes(['verify' => true]);
+
+    Route::get('/', '\Aimeos\Shop\Controller\CatalogController@homeAction')->name('aimeos_home');
+
+    Route::get('{path?}', '\Aimeos\Shop\Controller\PageController@indexAction')
+        ->name('aimeos_page')->where( 'path', '.*' );
+
+});
